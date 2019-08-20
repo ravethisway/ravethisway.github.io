4c5a4e00124e392fe880dc14fe57031b7f4ee793
Bela Lawson <bela@mvstudio.be>
2019-08-20 11:43:45 +0200

    delete beep trigger mp3

--- terminal.js
+++ terminal.js
@@ -81,12 +81,12 @@ var Terminal = (function () {
 	var terminalBeep
 
 	var TerminalConstructor = function (id) {
-		if (!terminalBeep) {
-			terminalBeep = document.createElement('audio')
-			var source = '<source src="http://www.erikosterberg.com/terminaljs/beep.'
-			terminalBeep.innerHTML = source + 'mp3" type="audio/mpeg">' + source + 'ogg" type="audio/ogg">'
-			terminalBeep.volume = 0.05
-		}
+		// if (!terminalBeep) {
+		// 	terminalBeep = document.createElement('audio')
+		// 	var source = '<source src="http://www.erikosterberg.com/terminaljs/beep.'
+		// 	terminalBeep.innerHTML = source + 'mp3" type="audio/mpeg">' + source + 'ogg" type="audio/ogg">'
+		// 	terminalBeep.volume = 0.05
+		// }
 
 		this.html = document.createElement('div')
 		this.html.className = 'Terminal'
@@ -100,10 +100,10 @@ var Terminal = (function () {
 
 		this._shouldBlinkCursor = true
 
-		this.beep = function () {
-			terminalBeep.load()
-			terminalBeep.play()
-		}
+		// this.beep = function () {
+		// 	terminalBeep.load()
+		// 	terminalBeep.play()
+		// }
 
 		this.print = function (message) {
 			var newLine = document.createElement('div')
