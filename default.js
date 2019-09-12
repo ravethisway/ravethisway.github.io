acb8c34de4f0aa2545ca512cec0afcda182beefc
Bela Lawson <bela@mvstudio.be>
2019-09-12 15:15:42 +0200

    fix

--- default.js
+++ default.js
@@ -19,6 +19,18 @@ function socialLogin() {
         }).catch((err) => {
             alert(err)
         });
+}
+
+function isset (accessor) {
+    try {
+        // Note we're seeing if the returned value of our function is not
+        // undefined
+        return typeof accessor() !== 'undefined'
+    } catch (e) {
+        // And we're able to catch the Error it would normally throw for
+        // referencing a property of undefined
+        return false
     }
+}
 
 
