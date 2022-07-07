61dffeb3a3a176fbafe4650637246500dd28236b
ravethisway <54139785+ravethisway@users.noreply.github.com>
2022-07-07 20:30:44 +0200

    Update default.js

--- default.js
+++ default.js
@@ -31,30 +31,28 @@ function getLocation(psw) {
     response['desc'] = ' '
     
     async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    //redirect: 'follow', // manual, *follow, error
-    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response; // parses JSON response into native JavaScript objects
-}
+      const response = await fetch(url, {
+        method: 'POST', 
+        mode: 'cors', ]
+        cache: 'default',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        //redirect: 'follow',
+        //referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+      });
+    return response;
+    }
 
-postData('https://waterfull.netlify.app/api/checkpsw', { "checkThis": psw })
-  .then(data => {
-    console.log('yo')
-    console.log(data)
-    response['desc'] = JSON.stringify(data)
-    response['loca'] = ' '
-  });
+    postData('https://waterfull.netlify.app/api/checkpsw', { "checkThis": psw })
+        .then(data => {
+            console.log('yo')
+            console.log(data)
+            response['desc'] = JSON.stringify(data)
+            response['loca'] = ' '
+        });
     return response
     /*
     let response = []
